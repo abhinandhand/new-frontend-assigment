90f68a0c9f83392978f6628701ff221aab90165a
adevadiga <abhinandhan.d@gmail.com>
2021-04-17 13:46:28 +0200

    Vuex Set up in stores & Fetch the Campaign Data

---
+++ fe-challenge/pages/campaigns/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>Welcome</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  async fetch({ store, error}){
+    try {
+      await store.dispatch('campaigns/fetchCampaigns');
+    }catch(err){
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
---
+++ fe-challenge/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import * as campaigns from './campaigns';
+
+Vue.use(Vuex);
+
+export const state = () => ({
+
+})
+
+export const mutations = {
+}
